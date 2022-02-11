f812681c63400f7ed8989c7ac066942150923316
Natusick <natallia.daniluk@gmail.com>
2022-02-11 20:26:08 +0200

    11/02

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,35 @@
-import throttle from "lodash.throttle";
-
-const refs = { 
+import throttle from 'lodash.throttle';
+const refs = {
   formEl: document.querySelector('.feedback-form'),
   inputEl: document.querySelector('.feedback-form input'),
   textareaEl: document.querySelector('.feedback-form textarea'),
 };
-const STORAGE_KEY = "feedback-form-state";
-
-getSaveInput();
-
+const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
-refs.formEl.addEventListener('input', throttle(currentInput, 500));
-refs.formEl.addEventListener('submit', onFormSubmit);
-
 const currentInput = function (e) {
   formData[e.target.name] = e.target.value;
   const formJSON = JSON.stringify(formData);
   localStorage.setItem(STORAGE_KEY, formJSON);
 };
-
-
-function getSaveInput(event){
-  const saveMessage = localStorage.getItem(STORAGE_KEY);
-  const saveMessageJson = JSON.parse(saveMessage);
-  
-  if(saveMessageJson){
-    refs.inputEl.value = saveMessageJson.email || '';
-    refs.textareaEl.value = saveMessageJson.message || '';
-  };
-  
-};
-
-function onFormSubmit(event){
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.formEl.addEventListener('input', throttle(currentInput, 500));
+getSaveInput();
+function onFormSubmit(event) {
   event.preventDefault();
-  if (refs.inputEl.value === "" || refs.textareaEl.value === "") {
-    return (alert ("Please fill in all the fields!"));
-  };
+  if (refs.inputEl.value === '' || refs.textareaEl.value === '') {
+    return alert('Please fill in all the fields!');
+  }
   localStorage.removeItem(STORAGE_KEY);
-  event.target.reset();
-};
+  refs.formEl.reset();
+  formData = {};
+}
+function getSaveInput() {
+  let saveMessage = localStorage.getItem(STORAGE_KEY);
+  if (saveMessage) {
+    saveMessage = JSON.parse(saveMessage);
+    Object.entries(saveMessage).forEach(([key, value]) => {
+      formData[key] = value;
+      refs.formEl.elements[key].value = value;
+    });
+  }
+}
